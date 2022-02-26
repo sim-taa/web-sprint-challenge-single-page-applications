c553a91480f3cb54877e5191f4ac07e2451918f3
Simone Taarnskov <simone.taarnskov@gmail.com>
2022-02-25 16:06:52 -0800

    Adds dependencies. Sets up App routes.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React from "react";
-
+import React from 'react';
+import { Switch, Route, Link } from 'react-router-dom';
 const App = () => {
+  console.log("secret message")
   return (
-    <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
+    <div clasName="App">
+    <header>
+      <Link to="/">Home</Link>
+      </header>
+      <Switch>
+        <Route exact path="/">
+        <h1>Okay Bloomer, Eat</h1>
+      <p>Nom nom nom nom</p>
+      <p>Nom nom nom nom</p>
+      <p>Nom nom nom nom</p>
+        </Route>
+      </Switch>
+      </div>
   );
 };
 export default App;
