202cde4222acc4ab58fd4f45a256a9b2222ee23a
Simone Taarnskov <simone.taarnskov@gmail.com>
2022-03-02 14:55:04 -0800

    adds name imput in form and passes associated test.

--- src/components/PizzaForm.js
+++ src/components/PizzaForm.js
@@ -1,13 +1,28 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { useParams } from "react-router-dom";
+import schema from '../validation/formSchema';
+import * as yup from 'yup';
 
 const PizzaForm = () => {
     const { type } = useParams()
 
+    const [form, setForm] = useState({
+        name: '',
+        size: '',
+        olives: false,
+        mushrooms: false,
+        onions: false,
+        pineapple: false,
+        instructions: '',
+    })
+
     return(<article>
     <h1>Type {type} order form.</h1>
     <form id="pizza-form">
-
+        <label>
+            Enter your name
+            <input type="text" name="name" id= "name-input"></input>;
+        </label>
     </form>
     </article>)
 }
