b1d01fa4f7572fb241555347ca4a1e9c013ff675
Simone Taarnskov <simone.taarnskov@gmail.com>
2022-03-02 16:35:29 -0800

    adjusts dropdown & checkboxes to pass tests.

--- src/components/PizzaForm.js
+++ src/components/PizzaForm.js
@@ -2,9 +2,24 @@ import React, {useState, useEffect} from "react";
 import { useParams } from "react-router-dom";
 import schema from '../validation/formSchema';
 import * as yup from 'yup';
+export default function PizzaForm(props) {
+    // const {
+    // /  values,
+    //   submit,
+    //   change,
+    //   disabled,
+    //   errors,
+    // } = props
 
-const PizzaForm = () => {
-    const { type } = useParams()
+
+  console.log(props)
+    const onSubmit = evt => {
+      evt.preventDefault()
+      submit()
+    }
+
+// const PizzaForm = () => {
+     const { type } = useParams()
 
     const [form, setForm] = useState({
         name: '',
@@ -21,14 +36,29 @@ const PizzaForm = () => {
     <form id="pizza-form">
         <label>
             Enter your name
-            <input type="text" name="name" id= "name-input"></input>;
+            <input type="text" name="name" id="name-input"></input>;
         </label>
         <label>
             Enter your special instructions
             <input type="text" name="instructions" id= "special-text"></input>
         </label>
+        <label>Select a pizza size</label>
+            <select 
+            id="size-dropdown"
+            name="size"
+            value=''
+            >
+            <option value=''>- Select an option -</option>
+            <option value='small'>small</option>
+            <option value='medium'>medium</option>
+            <option value='large'>large</option>
+            </select>
+            <h4>Toppings</h4>
+            <label><input type="checkbox" value="olives"></input></label>
+            <label><input type="checkbox" value="mushrooms"></input></label>
+            <label><input type="checkbox" value="onions"></input></label>
+            <label><input type="checkbox" value="pinapple"></input></label>
+            
     </form>
-    </article>)
-}
-
-export default PizzaForm;
+    </article>
+    )}
---
+++ src/validation/formSchema.js
@@ -0,0 +1,20 @@
+import * as yup from 'yup';
+
+const formSchema = yup.object().shape({
+  name: yup
+    .string()
+    .trim()
+    .required('name is required')
+    .min(2, 'name must be at least 2 characters'),
+  size: yup
+    .string()
+    .oneOf(['small', 'medium', 'large'], 'pick a size'),
+  olives: yup.boolean(),
+  mushrooms: yup.boolean(),
+  olives: yup.boolean(),
+  pinapple: yup.boolean(),
+  instructions: yup
+  .string()
+})
+
+export default formSchema;
